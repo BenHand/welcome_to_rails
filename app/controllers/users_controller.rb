8c7e57d4f9e21388ddbae7391082e9f7005ec91d
Benjamin Hand <gneeshot@gmail.com>
2015-06-02 13:18:12 -0500

    users controller now contains index and show method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,21 @@
 class UsersController < ApplicationController
 
   def index
-    users = User.all
-
-    render text: "testing"
+    all_users = User.all
+    string_users = all_users.map do |user|
+       "#{user.first_name} #{user.last_name}, #{user.age} y/o\n"
+    end
+    join = "_" * 20
+    render text: "#{join}#{string_users.join(join)}"
   end
 
   def show
+    if User.exists?(params[:id])
+      show_user = User.find(params[:id])
+      render text: "#{show_user.first_name} #{show_user.last_name}, #{show_user.age} y/o"
+    else
+      render text: "User Not Found", status: 404
+    end
   end
+
 end
