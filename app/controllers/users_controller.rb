3c49fe655bbcfdbd6f4dc0256f0892083fc52134
Benjamin Hand <gneeshot@gmail.com>
2015-06-02 15:05:39 -0500

    create method now functioning properly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,23 +2,28 @@ class UsersController < ApplicationController
 
   def index
     if params.length <=2
-      all_users = User.all
-      string_users = all_users.map do |user|
-        "(#{user.id}) #{user.first_name} #{user.last_name}, #{user.age} y/o\n"
-        end
-      render text: string_users.join('</p><p>')
+      display_users(User.all)
 
-    else
+    elsif params.include?("first_name")
       specific_users = User.where("first_name LIKE (?)", "#{params[:first_name]}%")
 
       if specific_users.length < 1
         render text: "User(s) Not Found", status: 404
       else
-        string_users = specific_users.map do |user|
-          "(#{user.id}) #{user.first_name} #{user.last_name}, #{user.age} y/o\n"
-          end
-        render text: string_users.join('</p><p>')
+        display_users(specific_users)
       end
+
+    elsif params.include?("limit") && params.include?("offset")
+      display_users(User.all.limit(params[:limit]).offset(params[:offset]))
+
+    end
+  end
+  def delete
+    if params[:action] == 'delete' && (User.exists?(params[:id])) == true
+      user = User.find(params[:id])
+      render text: "(#{user.id}) #{user.first_name} #{user.last_name} : DELETED"
+      User.destroy(params[:id])
+    else render text: "User Not Found ", status: 404
     end
 
   end
@@ -30,6 +35,22 @@ class UsersController < ApplicationController
     else
       render text: "User Not Found", status: 404
     end
+  end
+
+  def create
+    if params[:action] == 'create'
+      new_user = User.create(first_name: params[:first_name], last_name: params[:last_name], age: params[:age])
+      render text: "(#{new_user.id}) #{new_user.first_name} #{new_user.last_name} added to database."
+    else
+      render text: "errrr"
+    end
+  end
+
+  def display_users(passed_value)
+      string_users = passed_value.map do |user|
+        "(#{user.id}) #{user.first_name} #{user.last_name}, #{user.age} y/o\n"
+        end
+      render text: string_users.join('</p><p>')
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-  # get '/users?', to: 'users#parameters'
   get '/users', to: 'users#index'
+  get '/users/create', to: 'users#create'
   get '/users/:id', to: 'users#show'
-
+  get '/users/:id/delete', to: 'users#delete'
 
 end
