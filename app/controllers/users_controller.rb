d5cbafc43eb428d9fc5144ad44c6ec1e342ed781
Benjamin Hand <gneeshot@gmail.com>
2015-06-02 13:03:50 -0500

    added faker gem, generated controller and added 50 users to db

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
 
   def index
+    users = User.all
+
+    render text: "testing"
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  # get '/users', to: 'users#index'
+  get '/users', to: 'users#index'
   # get '/users/:id', to: 'users#show'
 
 end
